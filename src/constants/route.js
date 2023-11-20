7bfd99a4a2dabecd718fc2278caf3e98fd17e4d2
mahadevlover <sukhveerbhatigulshan@gmail.comr>
2023-11-20 23:24:46 +0530

    updated app path

--- src/constants/route.js
+++ src/constants/route.js
@@ -1,8 +1,5 @@
-const APP_PATH = '/imdb';
-
-
 export const routhPath = {
-    home                    :  `${APP_PATH}/`,
-    categories              :  `${APP_PATH}/categories`,
-    invalid                 :  `${APP_PATH}/*`
+    home                    :  `/`,
+    categories              :  `/categories`,
+    invalid                 :  `*`
 }
